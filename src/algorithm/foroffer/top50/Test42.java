93ba8ae982e4c5230616d7cd82d70b8ab824e759
lah0907 <yazhou6543@gmail.com>
2017-06-02 15:02:25 +0800

    for offer 2017-6-2 15:02:23

---
+++ src/algorithm/foroffer/top50/Test42.java
@@ -0,0 +1,25 @@
+package algorithm.foroffer.top50;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @create 2017-06-02 14:57
+ *
+ * 面试题42：翻转单词顺序 VS 左旋转字符串
+ *
+ * 题目一：
+ *      输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+ *      为简单起见，标点符号和普通字母一样处理。
+ *      例如输入字符串"I am a student."，则输出"student. a am I"。
+ *
+ * 考查点：
+ *
+ * 思路：
+ *
+ * 题目二：
+ *
+ *
+ */
+public class Test42 {
+}
