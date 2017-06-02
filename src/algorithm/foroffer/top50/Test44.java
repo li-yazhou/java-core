2a04951ce28522a7bc71eea389e417f7159d01ad
lah0907 <yazhou6543@gmail.com>
2017-06-02 20:13:48 +0800

    for offer 2017-6-2 20:13:46

---
+++ src/algorithm/foroffer/top50/Test44.java
@@ -0,0 +1,19 @@
+package algorithm.foroffer.top50;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @create 2017-06-02 20:12
+ *
+ * 面试题44：扑克牌的顺子
+ *
+ * 题目：
+ *
+ * 考查点：
+ *
+ * 思路：
+ *
+ */
+public class Test44 {
+}
