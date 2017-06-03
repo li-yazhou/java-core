d96274e4d4f948c448ebea74a9733289825fa93d
lah0907 <yazhou6543@gmail.com>
2017-06-03 22:18:16 +0800

    for offer 2017-6-3 22:18:13

---
+++ src/algorithm/foroffer/top50/Test45.java
@@ -0,0 +1,49 @@
+package algorithm.foroffer.top50;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @create 2017-06-03 20:41
+ * 面试题 46：圆圈中最后剩下的数字
+ *
+ * 题目：
+ *      0, 1, 2, ..., n-1 这 n 个数字排成一个圆圈，从数字 0 开始每次从这个圆圈里删除第 m 个数字。
+ *      求出这个圆圈里剩下的最后一个数字。
+ *
+ * 考查点：
+ *      1. 约瑟夫环问题
+ *
+ * 思路：
+ *      1. 圆形链表
+ *
+ *
+ */
+public class Test45 {
+
+    public int lastRemainElement(int n, int m){
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < n; i ++) list.add(i);
+
+        int idx = 0;
+        for (int i = 0; i < n-1; i ++){
+            // for (int j = 0; j < m-1; j ++) idx ++;
+            idx = idx + (m-1);
+            idx = idx % list.size();
+            list.remove(idx);
+        }
+        return list.get(0);
+    }
+
+    @Test
+    public void test(){
+        System.out.println(lastRemainElement(5, 3));
+    }
+
+
+}
---
+++ src/algorithm/foroffer/top50/Test47.java
@@ -0,0 +1,21 @@
+package algorithm.foroffer.top50;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @create 2017-06-03 22:01
+ *
+ * 面试题 47：不用加减乘除做加法
+ *
+ * 题目：
+ *      写一个函数，求两个整数之和，要求在函数体内不得使用加减乘除四则运算。
+ *
+ * 考查点：
+ *
+ * 思路：
+ *
+ *
+ */
+public class Test47 {
+}
