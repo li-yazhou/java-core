fd22b7fcacf81d82a0b50409bfad8356a7a10229
lah0907 <yazhou6543@gmail.com>
2017-06-06 20:49:46 +0800

    for offer 2017-6-6 20:49:44

--- src/algorithm/foroffer/top60/Test51.java
+++ src/algorithm/foroffer/top60/Test51.java
@@ -1,5 +1,10 @@
 package algorithm.foroffer.top60;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
 /**
  * description:
  *
@@ -14,12 +19,50 @@ package algorithm.foroffer.top60;
  *      如果输入长度为 7 的数组 {2, 3, 1, 0, 2, 5, 3}，那么对应
  *      的输出是重复的数字 2 或者 3。
  *
- *
  * 考查点：
  *
- *
  * 思路：
+ *      1. 方法一：
+ *          时间复杂度小，需要辅助空间
+ *          需要一个保存数字以及其出现次数的 map
+ *      2. 方法二：
+ *           时间复杂度高，空间复杂度是 O(1)
+ *           两层循环遍历所有的数字
  *
  */
 public class Test51 {
+
+    public int findOneDuplicateDigit(int[] array){
+        if (array == null || array.length == 0) throw new RuntimeException("Invalid input.");
+        // Map<Integer, Integer> map = new HashMap<>();
+        int[] entrys = new int[array.length];
+        int result = -1;
+        for (int i = 0; i < array.length; i++){
+            entrys[array[i]] += 1;
+            if (entrys[array[i]] > 1) {
+                result = array[i];
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(entrys));
+        return result;
+    }
+
+    public int findOneDuplicateDigit2(int[] array){
+        if (array == null || array.length == 0) throw new RuntimeException("Invalid input.");
+        int result = -1;
+        for (int i = 0; i < array.length; i ++){
+            for (int j = i+1; j < array.length; j ++){
+                if (array[i] == array[j]) return array[i];
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void test(){
+        int[] array = {1, 2, 3, 5, 7, 2, 1, 3};
+        System.out.println(findOneDuplicateDigit(array));
+        System.out.println(findOneDuplicateDigit2(array));
+    }
 }
