6fe0f50609ca82f7563a2b0513a0b3b10330e8bd
lah0907 <yazhou6543@gmail.com>
2017-07-16 22:31:31 +0800

    spring tutorial

---
+++ src/javacore/functions/Utils.java
@@ -0,0 +1,10 @@
+package javacore.functions;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-16 11:13
+ */
+public class Utils {
+}
---
+++ src/spring/spring_tutorial/test/base/UnitTestBase.java
@@ -0,0 +1,43 @@
+package spring.spring_tutorial.test.base;
+
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.StringUtils;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-16 22:24
+ */
+public class UnitTestBase {
+    private ClassPathXmlApplicationContext context;
+    private String springXmlPath;
+    public UnitTestBase(){}
+    public UnitTestBase(String _springXmlPath){
+        springXmlPath = _springXmlPath;
+    }
+
+    @Before
+    public void before(){
+        if (StringUtils.isEmpty(springXmlPath)){
+            springXmlPath = "classpath*:spring-*xml";
+        }
+        context = new ClassPathXmlApplicationContext(springXmlPath.split("[,\\s]+"));
+        context.start();
+    }
+
+    @After
+    public void after(){
+        context.destroy();
+    }
+
+    protected <T extends Object> T getBean(String beanId){
+        return (T) context.getBean(beanId);
+    }
+
+    protected <T extends Object> T getBean(Class<T> clazz){
+        return context.getBean(clazz);
+    }
+}
