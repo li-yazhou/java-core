3d48926f100b4a17de6986098981c0568a683ec2
lah0907 <yazhou6543@gmail.com>
2017-07-19 22:29:26 +0800

    spring ioc, aop

---
+++ java/javacore/book/fkjava/ch14_annotation/demo2/Query.java
@@ -0,0 +1,10 @@
+package javacore.book.fkjava.ch14_annotation.demo2;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-19 14:07
+ */
+public interface Query {
+}
---
+++ spring/main/java/org/spring/bean/javabased/StoreConfig.java
@@ -0,0 +1,10 @@
+package org.spring.bean.javabased;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-19 15:01
+ */
+public class StoreConfig {
+}
