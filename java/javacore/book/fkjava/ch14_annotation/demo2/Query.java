84b11fb87525db12dcf7ccdb2480e34fcf44d4f2
lah0907 <yazhou6543@163.com>
2017-07-20 22:30:46 +0800

    adjust package structure

--- java/javacore/book/fkjava/ch14_annotation/demo2/Query.java
+++ java/javacore/book/fkjava/ch14_annotation/demo2/Query.java
@@ -7,4 +7,6 @@ package javacore.book.fkjava.ch14_annotation.demo2;
  * @since 2017-07-19 14:07
  */
 public interface Query {
+    void query();  // 严格查询，相等查询
+    void likeQuery();  // 模糊查询，like查询
 }
--- java/javacore/book/fkjava/ch14_annotation/demo2/Test2.java
+++ java/javacore/book/fkjava/ch14_annotation/demo2/Test2.java
@@ -1,6 +1,5 @@
 package javacore.book.fkjava.ch14_annotation.demo2;
 
-import com.sun.xml.internal.bind.v2.TODO;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
@@ -42,38 +41,57 @@ public class Test2 {
      */
     private String query(Filter filter){
         StringBuilder sql = new StringBuilder();
-
         Class clazz = filter.getClass();
+
+        // 表名
         boolean exists = clazz.isAnnotationPresent(Table.class);
         if (!exists) return null;
         Table table = (Table) clazz.getAnnotation(Table.class);
         sql.append("select * from ").append(table.value()).append(" where 0 = 0");
 
+        // 字段
         Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields){
-            boolean columnExists = field.isAnnotationPresent(Column.class);
-            if (!columnExists) continue;
-            Column column = field.getAnnotation(Column.class);
-            String columnName = column.name();
-            String columnType = column.type();
-
-            String fieldName = field.getName();
-            String getMethodName = "get" + fieldName.substring(0,1).toUpperCase() + fieldName.substring(1);
-            String columnValue = null;
-            try {
-                Method getMethod = clazz.getMethod(getMethodName);
-                columnValue = "" + getMethod.invoke(filter);
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-
-            // TODO, 整型字段不能等于0，若等于则忽略
-            if ("null".equals(columnValue) || "0".equals(columnValue)) continue;
-            if ("string".equals(columnType)) sql.append(" and ").append(columnName).append(" = %").append(columnValue);
-            else sql.append(" and ").append(columnName).append(" = ").append(columnValue);
-
-        }
+        for (Field field : fields)
+            combineSql(sql, clazz, field, filter);
 
         return sql.toString();
     }
+
+    private void combineSql(StringBuilder sql, Class clazz, Field field, Filter filter){
+        boolean columnExists = field.isAnnotationPresent(Column.class);
+        if (!columnExists) return;
+        Column column = field.getAnnotation(Column.class);
+        String columnName = column.name();  // 该field对应的表中的列名
+        String columnType = column.type();
+
+        // 获取field的值
+        String fieldName = field.getName();
+        String getMethodName = "get" + fieldName.substring(0,1).toUpperCase() + fieldName.substring(1);
+        String columnValue = "";
+        try {
+            Method getMethod = clazz.getMethod(getMethodName);
+            columnValue += getMethod.invoke(filter);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        // TODO, 整型字段不能等于0，若等于则忽略
+        if ("null".equals(columnValue) || "0".equals(columnValue)) return;
+
+        if ("int".equals(columnType)) sql.append(" and ").append(columnName).append(" = ").append(columnValue);
+
+        if ("string".equals(columnType)) {
+            if (columnValue.contains(",")){
+                sql.append(" and ").append(columnName).append(" in(");
+                String[] values = columnValue.split(",");
+                for (String value : values)
+                    sql.append("`").append(value).append("`").append(",");
+                sql.deleteCharAt(sql.length()-1);
+                sql.append(")");
+            }else{
+                sql.append(" and ").append(columnName).append(" = `").append(columnValue).append("`");
+            }
+        }
+    }
+
 }
