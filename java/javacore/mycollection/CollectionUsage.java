9067835cdfa30bfc06148f57e3bee6375e52b8a6
lah0907 <yazhou6543@163.com>
2017-07-23 22:39:17 +0800

    Java collection

--- java/javacore/mycollection/CollectionUsage.java
+++ java/javacore/mycollection/CollectionUsage.java
@@ -1,5 +1,9 @@
 package javacore.mycollection;
 
+import org.junit.Test;
+
+import java.util.*;
+
 /**
  * description:
  *
@@ -7,4 +11,42 @@ package javacore.mycollection;
  * @since 2017-07-22 10:46
  */
 public class CollectionUsage {
+
+    // ========================== java.util.Map ==========================
+    @Test
+    public void testMap(){
+        Map map = null;
+        HashMap hashMap = null;
+        LinkedHashMap linkedHashMap = null;
+        TreeMap treeMap = null;
+
+        hashMap = new HashMap();
+        hashMap.put(null, "2");
+        hashMap.put(null, "22");
+        System.out.println(hashMap.size());
+    }
+
+
+    // ========================== java.util.Collection ==========================
+
+    public void testList(){
+        List list = null;
+        ArrayList arrayList = null;
+        LinkedList linkedList = null;
+    }
+
+    public void testSet(){
+        Set set = null;
+        HashSet hashSet = null;
+        LinkedHashSet linkedHashSet = null;
+        TreeMap treeMap = null;
+    }
+
+    public void testQueue(){
+        Queue queue = null;
+        PriorityQueue priorityQueue = null;
+        ArrayDeque arrayDeque = null;
+        LinkedList linkedList = null;
+    }
+
 }
