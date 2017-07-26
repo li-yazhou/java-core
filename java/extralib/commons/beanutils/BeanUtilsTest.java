3d075752ea47ad8699d368e2a758ad481fa05fce
lah0907 <yazhou6543@gmail.com>
2017-07-26 19:37:34 +0800

    DbUtils usage

---
+++ java/extralib/commons/beanutils/BeanUtilsTest.java
@@ -0,0 +1,17 @@
+package extralib.commons.beanutils;
+
+import org.junit.Test;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-26 16:44
+ */
+public class BeanUtilsTest {
+
+    @Test
+    public void test01(){
+        
+    }
+}
---
+++ java/extralib/commons/beanutils/PropertyDescriptorTest.java
@@ -0,0 +1,10 @@
+package extralib.commons.beanutils;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-26 16:44
+ */
+public class PropertyDescriptorTest {
+}
