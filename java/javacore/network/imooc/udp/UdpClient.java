c4462421d738e28820467c07e28204390a829e09
lah0907 <yazhou6543@gmail.com>
2017-07-26 08:33:40 +0800

    socket programming...

--- java/javacore/network/imooc/udp/UdpClient.java
+++ java/javacore/network/imooc/udp/UdpClient.java
@@ -14,6 +14,9 @@ import java.net.InetAddress;
 public class UdpClient {
 
     public static void main(String[] args) throws IOException {
+        /**
+         * 向服务器端发送数据
+         */
         // 1. 定义服务器的地址、端口号、数据
         InetAddress address = InetAddress.getByName("localhost");
         int port = 3333;
@@ -25,6 +28,23 @@ public class UdpClient {
         DatagramSocket datagramSocket = new DatagramSocket();
         datagramSocket.send(packet);
 
+
+        /**
+         * 接收服务器端发过来的数据
+         */
+        // 定义数据报
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        // 接收服务器发送的数据
+        datagramSocket.receive(receivePacket);
+
+        // 读取接收到的数据
+        String line = new String(receiveData, 0, receiveData.length);
+        System.out.println("我是客户端，服务器端发送的数据是：" + line);
+
+        // 关闭资源
+        datagramSocket.close();
     }
 
 
