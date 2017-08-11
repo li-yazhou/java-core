bb5c12410db2c9791511ac56ee2187382997d598
lah0907 <yazhou6543@gmail.com>
2017-08-11 22:24:37 +0800

    Java 基础强化

---
+++ java/javaenhance/part01/EnumTest.java
@@ -0,0 +1,112 @@
+package javaenhance.part01;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-08-11 21:31
+ */
+
+/**
+ * 带有抽象方法的枚举类
+ */
+enum WeekDay{
+    SUNDAY(0){
+        @Override
+        public WeekDay nextDay() {
+            return MONDAY;
+        }
+    }, MONDAY(1){
+        @Override
+        public WeekDay nextDay() {
+            return TUESDAY;
+        }
+    }, TUESDAY(2) {
+        @Override
+        public WeekDay nextDay() {
+            return WEDNESDAY;
+        }
+    }, WEDNESDAY(3) {
+        @Override
+        public WeekDay nextDay() {
+            return THURSDAY;
+        }
+    }, THURSDAY(4) {
+        @Override
+        public WeekDay nextDay() {
+            return FRIDAY;
+        }
+    }, FRIDAY(5) {
+        @Override
+        public WeekDay nextDay() {
+            return SATURDAY;
+        }
+    }, SATURDAY(6) {
+        @Override
+        public WeekDay nextDay() {
+            return SUNDAY;
+        }
+    };
+    int day;
+    WeekDay(int day){
+        this.day = day;
+    }
+
+    public int day(){
+        return day;
+    }
+    public abstract WeekDay nextDay();
+}
+
+
+enum TrafficLamp{
+    RED(20) {
+        @Override
+        public TrafficLamp nextLamp() {
+            return GREEN;
+        }
+    },
+    GREEN(60) {
+        @Override
+        public TrafficLamp nextLamp() {
+            return YELLOW;
+        }
+    },
+    YELLOW(10) {
+        @Override
+        public TrafficLamp nextLamp() {
+            return RED;
+        }
+    };
+
+    int time;
+    TrafficLamp(int time){
+        this.time = time;
+    }
+
+    public abstract TrafficLamp nextLamp();
+}
+
+public class EnumTest {
+
+    @Test
+    public void test(){
+        WeekDay weekDay = WeekDay.TUESDAY;
+        WeekDay nextDay = weekDay.nextDay();
+        System.out.println(weekDay);
+        System.out.println(nextDay);
+
+        WeekDay weekDay1 = WeekDay.valueOf("FRIDAY");
+        System.out.println(weekDay1);
+        System.out.println(weekDay1.name());
+        System.out.println(weekDay1.ordinal());
+        System.out.println(weekDay1.day());
+
+        WeekDay[] weekDays = WeekDay.values();
+        System.out.println(Arrays.toString(weekDays));
+    }
+}
---
+++ java/javaenhance/part01/VariableParameter.java
@@ -0,0 +1,27 @@
+package javaenhance.part01;
+
+import org.junit.Test;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-08-11 20:11
+ */
+public class VariableParameter {
+
+    public int add(int... args){
+        int sum = 0;
+        for (int i : args) sum += i;
+        return sum;
+    }
+
+    @Test
+    public void addTest(){
+        int result = add();
+        System.out.println(result);
+
+        result = add(1,2,3,4);
+        System.out.println(result);
+    }
+}
