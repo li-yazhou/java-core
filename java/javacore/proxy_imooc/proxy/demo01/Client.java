2151c02814bf73fb6f7d6388d0051eb8c4112406
lah0907 <yazhou6543@gmail.com>
2017-08-22 14:53:48 +0800

    cglib usage

---
+++ java/javacore/proxy_imooc/proxy/demo01/Client.java
@@ -0,0 +1,33 @@
+package javacore.proxy_imooc.proxy.demo01;
+
+import org.junit.Test;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-08-22 11:13
+ */
+public class Client {
+
+    @Test
+    public void testCarMove(){
+        Movable car = new Car();
+        car.move();
+    }
+
+
+    @Test
+    public void testEnhanceCarMove(){
+        Movable car = new EnhanceCarByExtends();
+        car.move();
+    }
+
+    @Test
+    public void testEnhanceCarByCombination(){
+        Movable car = new Car();
+        Movable enhanceCar = new EnhanceCarByCombination(car);
+        enhanceCar.move();
+    }
+
+}
