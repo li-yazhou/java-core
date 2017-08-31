9f033c2d24e6039ad41f953c71d67ebbf1d316d6
lah0907 <yazhou6543@gmail.com>
2017-08-31 21:33:38 +0800

    美团笔试

---
+++ java/test/test20170831/meituan/test02/Main.java
@@ -0,0 +1,10 @@
+package test.test20170831.meituan.test02;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-08-31 18:51
+ */
+public class Main {
+}
---
+++ java/test/test20170831/meituan/test03/Main.java
@@ -0,0 +1,10 @@
+package test.test20170831.meituan.test03;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-08-31 18:51
+ */
+public class Main {
+}
