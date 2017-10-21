77130625a70c6a773de0bb154a96f21e97fb2a9c
lah0907 <yazhou6543@gmail.com>
2017-10-21 08:59:41 +0800

    update repo

---
+++ java-8/java8_in_action/chapter04/Dish.java
@@ -0,0 +1,86 @@
+package java8_in_action.chapter04;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * description:
+ *
+ * @author liyazhou
+ * @since 2017-07-28 21:08
+ */
+public class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", vegetarian=" + vegetarian +
+                ", calories=" + calories +
+                ", type=" + type +
+                '}';
+    }
+
+    public enum Type{ MEAT, FISH, OTHER }
+
+    public static final List<Dish> menu =
+            Arrays.asList(
+                    new Dish("pork", false, 800, Type.MEAT),
+                    new Dish("beef", false, 700, Type.MEAT),
+                    new Dish("chicken", false, 400, Type.MEAT),
+                    new Dish("french fries", true, 530, Type.OTHER),
+                    new Dish("rice", true, 350, Type.OTHER),
+                    new Dish("season fruit", true, 120, Type.OTHER),
+                    new Dish("pizza", true, 550, Type.OTHER),
+                    new Dish("prawns", false, 400, Type.FISH),
+                    new Dish("salmon", false, 450, Type.FISH)
+            );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
