9a25644d14d0d8255178169b1e9596f6eebe4377
lyz0907 <yazhou6543@gmail.com>
2018-07-10 19:17:01 +0800

    调整extension-jar模块的结构

---
+++ extension-jar/src/main/java/org/alpha/jodd/submodule/AppTest.java
@@ -0,0 +1,10 @@
+package org.alpha.jodd.submodule;
+
+/**
+ * <p>Description: <p/>
+ *
+ * @author: liyazhou1
+ * @date: 2018/7/10
+ */
+public class AppTest {
+}
---
+++ extension-jar/src/main/java/org/alpha/junit4/submodule/App.java
@@ -0,0 +1,10 @@
+package org.alpha.junit4.submodule;
+
+/**
+ * <p>Description: <p/>
+ *
+ * @author: liyazhou1
+ * @date: 2018/7/10
+ */
+public class App {
+}
