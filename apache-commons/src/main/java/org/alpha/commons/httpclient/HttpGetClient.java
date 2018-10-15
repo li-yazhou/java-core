a52e7608e6f9609cc5734ee3d2d9915560761f72
lyz0907 <yazhou6543@gmail.com>
2018-10-15 16:28:52 +0800

    apache-commons httpclient模块

---
+++ apache-commons/src/main/java/org/alpha/commons/httpclient/HttpGetClient.java
@@ -0,0 +1,24 @@
+package org.alpha.commons.httpclient;
+
+import org.apache.http.client.methods.HttpGet;
+
+/**
+ * @author liyazhou1
+ * @date 2018/9/21
+ */
+public class HttpGetClient {
+
+    private String url;
+
+    private HttpGet httpGet;
+
+    public HttpGetClient(String url){
+        this.url = url;
+    }
+
+    public String httpGet() {
+        return "";
+    }
+
+
+}
