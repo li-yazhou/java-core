68e5056a3aaf299b0214fcd1828e36cc708339a8
liyazhou <yazhou6543@gmail.com>
2019-03-26 20:43:15 +0800

    初始化 vertx-primer

---
+++ vertx-primer/src/main/java/vertx/helloworld/MainVerticle.java
@@ -0,0 +1,23 @@
+package vertx.helloworld;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startFuture.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startFuture.fail(http.cause());
+      }
+    });
+  }
+}
