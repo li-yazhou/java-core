a9ef6d487f78d5f47c35ce99650f5c367f71d1c2
liyazhou <yazhou6543@gmail.com>
2019-12-10 21:57:57 +0800

    update: init base module

---
+++ base/src/main/java/org/alpha/lang/StringUtil.java
@@ -0,0 +1,11 @@
+package org.alpha.lang;
+
+/**
+ *
+ * @author liyazhou1
+ * @date 2019/12/10
+ */
+public class StringUtil {
+
+}
+
---
+++ base/src/main/java/org/alpha/util/PropertiesUtil.java
@@ -0,0 +1,10 @@
+package org.alpha.util;
+
+/**
+ *
+ * @author liyazhou1
+ * @date 2019/12/10
+ */
+public class PropertiesUtil {
+
+}
